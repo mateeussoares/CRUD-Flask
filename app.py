7c396b72bd452f77d1958481e83105f5a9f20181
Mateus Soares <mateussoaresss13@gmail.com>
2026-04-24 21:23:20 -0300

    add read/get task endpoint

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ task_id_control = 1
 #Create
 @app.route("/tasks", methods=['POST'])
 def create_task():
+
     global task_id_control
     data = request.get_json() 
     new_task = Task(id=task_id_control, title=data.get('title'), description=data.get("description", ""))
@@ -20,6 +21,31 @@ def create_task():
     tasks.append(new_task)
     print(tasks)
     return jsonify({"message": "New task created successfully!"})
+
+#Read
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+
+    task_list = []
+    for task in tasks:
+        task_list.append(task.to_dict())
+    #Outra forma de fazer o código acima: task_list = [task.to_dict() for task in tasks] 
+
+    output = {
+            "tasks": task_list,
+            "total_tasks": len(task_list)
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = None 
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Could not find the activity."}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
